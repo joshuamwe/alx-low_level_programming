c0fed3ce3f93f97c1c11933c49d33861bb6382ad
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-10 09:22:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function used
+ *
+ * Return: 0 always when correct
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
