4642484f6ce94b615104cf009d037674492a941a
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-11 14:10:27 -0700

    nym

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: is 0 when correct
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0 ; k < 10; k++)
+		putchar('k');
+	putchar('\n');
+	return (0);
+}
