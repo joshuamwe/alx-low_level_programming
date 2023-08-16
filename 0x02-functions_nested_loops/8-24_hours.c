e603abd406f42c2406323a7783c5e230252c8def
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-16 11:37:58 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,15 +7,16 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0 ; i < 24 ; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0 ; j < 60 ; j++)
+		for (j = 0; j < 60; j++)
 		{
 			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
 			_putchar(':');
 			_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
