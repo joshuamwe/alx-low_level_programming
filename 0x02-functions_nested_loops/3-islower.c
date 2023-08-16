a507c397757d38964c43ce3f0c5344db321b170d
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-16 05:06:38 -0700

    slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_islower - checksfor lowercase character
+ *@c: checked parameter
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return(0);
+}
+
