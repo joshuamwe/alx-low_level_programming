076ba54cd321469362f62844ccd58b62116deb98
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-18 09:32:15 +0300

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include  "main.h"
+/**
+ * _isdigit themain function checks 0 through 9
+ * @c: function parameter
+ * Return: 1 succesfull or 0 fail
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
