a16faf3d66bdf0ea8565fa5b9d31b725622635cd
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-19 00:26:10 +0300

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal using the '\' character
+ * @n: function parameter
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j <= i ; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+	}
+}
