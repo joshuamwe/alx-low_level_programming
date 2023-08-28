4d5e52a6d1c93ab5af6e0b84fc6e3e6559000b58
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-28 16:23:28 +0300

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy - main function copies memory area
+ * @dest: function parameter 1
+ * @src: function parameter 2
+ * @n:functions parameter 3
+ * Return: pointer to dest
+ */
+
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *original_dest = dest;
