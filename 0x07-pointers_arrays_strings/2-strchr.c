b80fb144388865045133b9e2351e3f942757d1f7
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-28 16:32:28 +0300

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,8 +6,8 @@
  * @s: The string to search in
  * @c: The character to locate
  *
- * Return: A pointer to the first occurrence
- *         or NULL if the character is not found.
+ * Return: A pointer to the first occurrence of the character c
+ *           in the string s, or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
