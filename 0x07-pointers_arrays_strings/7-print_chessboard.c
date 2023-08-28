d70d8b00ba0e09686f52d13f93f923aa36abe48a
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-28 22:55:54 +0300

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Prints an 8x8 chessboard
+ * @a: Pointer to a 2D array representing the chessboard (unused)
+ *
+ * This function prints an 8x8 chessboard using alternating
+ * white and black squares.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	(void)a; /* Cast to void to indicate parameter is intentionally unused */
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if ((i + j) % 2 == 0)
+				putchar(32); /* White square */
+			else
+				putchar(35); /* Black square */
+		}
+		putchar('\n');
+	}
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char chessboard[8][8];
+
+	print_chessboard(chessboard);
+
+	return (0);
+}
