a6217b3f3db20975c20e2d0e6966aa3f89c2f5ec
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-30 19:07:00 +0300

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,8 +9,9 @@
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return 0;
-
-    return (n - 1);
+	if ((n % 2)  != 0 && (n % 3) != 0 && (n % 5) != 0 && (n % 1) == 0)
+	{
+		return (1);
+	}
+	return (0);
 }
