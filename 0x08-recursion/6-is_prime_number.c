621be33283e799a34a9d30f25e84396d3794b78b
joshuamwe <joshuajomwema2819@gmail.com>
2023-08-30 17:11:58 +0300

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdbool.h>
 /**
  * is_prime_number - Check if a number is prime.
  * @n: The number to check.
+ *@divisor: The divisor to check divisibility.
  * Return: 1 if @n is prime, 0 otherwise.
  */
-
 int is_prime_number(int n)
 {
-	int i = 5;
+    if (n <= 1)
+        return 0;
 
-	if (n <= 1)
-		return (0);
-
-	if (n <= 3)
-		return (1);
-
-	if (n % 2 == 0 || n % 3 == 0)
-		return (0);
-
-	while (i * i <= n)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
-
-		i += 6;
-	}
-
-	return (1);
- }
-
-
+    return (n - 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int start, int end);
-
 #endif 
