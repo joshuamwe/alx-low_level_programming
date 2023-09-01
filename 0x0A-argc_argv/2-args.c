30596bc9d579086e98e2dbdeb39e17ed50bb1c20
joshuamwe <joshuajomwema2819@gmail.com>
2023-09-01 14:41:02 +0300

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,14 @@
  * @argv: Array of command-line arguments
  * Return: 0
  */
-int main(int argc, char *argv[]) {
-    int i;
+int main(int argc, char *argv[])
+{
+	int i;
 
-    for (i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
-    }
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
-    return 0;
+	return (0);
 }
