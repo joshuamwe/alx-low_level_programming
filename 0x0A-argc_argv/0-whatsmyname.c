4c4e249e2085b828a19bdb3d16c0bb5a15048391
joshuamwe <joshuajomwema2819@gmail.com>
2023-09-01 14:28:21 +0300

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: parameter function 1
+ * @argv: parameter function 2
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
