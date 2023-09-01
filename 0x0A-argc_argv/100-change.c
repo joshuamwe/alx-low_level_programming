36b85405347d3dbefe32aefb6794626b3ccfde4d
joshuamwe <joshuajomwema2819@gmail.com>
2023-09-01 16:38:48 +0300

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,31 +9,31 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents = atoi(argv[1]);
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(coins) / sizeof(coins[0]);
-	int min_coins = 0;
-	int i = 0;
+	int cents, mncoin = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (cents < 0)
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
 	{
-		printf("0\n");
-		return (0);
+		if (cents >= 25)
+			cents -= 25;
+		if (cents >= 10)
+			cents -= 10;
+		if (cents >= 5)
+			cents -= 5;
+		if (cents >= 2)
+			cents -= 2;
+		if (cents >= 1)
+			cents -= 1;
+		mncoin += 1;
 	}
-	for (i = 0; i < num_coins; i++);
-	{
-		while (cents >= coins[i])
-		{
-			cents -= coins[i];
-			min_coins++;
-		}
-	}
-	printf("%d\n", min_coins);
+	printf("%d\n", mncoin);
 	return (0);
 }
 
