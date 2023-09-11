ff7896b0587d48b31617dacdcc8ed89825cec114
joshuamwe <joshuajomwema2819@gmail.com>
2023-09-11 17:53:51 +0300

    task 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,6 +4,7 @@
  * init_dog - main function
  * @d: pointer to dog
  * @name: name of dog
+ * @age: age of dog
  * @owner: owner name
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
