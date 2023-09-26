64b294feb57bf9a902c372c0323733cdb4b07401
joshuamwe <joshuajomwema2819@gmail.com>
2023-09-26 13:48:15 +0300

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,10 +16,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (i == index)
 		{
-			return head;
+			return (head);
 		}
 		head = head->next;
 		i++;
 	}
-	return NULL;
+	return (NULL);
 }
