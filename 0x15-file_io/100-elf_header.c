baf0b9158f751b2fede0431eab2642403698332c
joshuamwe <joshuajomwema2819@gmail.com>
2023-10-17 11:17:32 +0300

    sss

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <elf.h>
+void print_elf_header_info
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,4 +10,18 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
  void error_exit(int code, const char *format, ...);
+ void error_exit(const char *message):
+
+#define ELF_HEADER_SIZE 64
+/**
+ * error_exit - main function
+ * @message: parameter 1
+ * Return: void
+ */
+void error_exit(const char *message) 
+{
+	fprintf(stderr, "Error: %s\n", message);
+	exit(98);
+}
+
 #endif
