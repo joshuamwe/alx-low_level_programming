453ad2cd32d859a5f99c68adef8ab3d6060f2ca4
joshuamwe <joshuajomwema2819@gmail.com>
2023-10-30 13:36:56 +0300

    task fileio

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,27 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
 #include <stddef.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <stdio.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-void error_exit(int code, const char *format, ...);
-void error_exit(const char *message);
-
 #define ELF_HEADER_SIZE 64
-/**
- * error_exit - main function
- * @message: parameter 1
- * Return: void
- */
-void error_exit(const char *message)
-{
-	fprintf(stderr, "Error: %s\n", message);
-	exit(98);
-}
-
+#define ERROR "Error: can't read from file %s\n"
+#define ERR "Error: can't write to %s/n"
 #endif
